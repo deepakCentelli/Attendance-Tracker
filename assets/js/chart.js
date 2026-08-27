488351df4f34068909606f5d0f74c37d7e8cbdf0
deepakCentelli <deepak.sharma@centelli.com>
2026-08-27 18:05:20 +0530

    Update chart.js

---
+++ assets/js/chart.js
@@ -0,0 +1,6 @@
+const ChartRenderer = {
+    draw(canvasId, percentage, color) {
+        const ctx = document.getElementById(canvasId).getContext('2d');
+        // Clear and redraw arc logic here
+    }
+};
